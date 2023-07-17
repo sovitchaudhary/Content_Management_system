04f929a62ec1a4ab378a1e71b1cc2ed3def52ec5
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-07-17 11:16:44 +0530

    add dbh.php

---
+++ admin/includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+    $dbservername = "localhost";
+    $dbuser = "root";
+    $dbpassword = "";
+    $dbname = "cms";
+
+    $conn = mysqli_connect($dbservername,$dbuser,$dbpassword,$dbname);
+?>
