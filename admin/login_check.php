cd1d2c7a299ef981bb086f5a0c353fe2331817e5
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-07-17 18:02:13 +0530

    add new file

---
+++ admin/login_check.php
@@ -0,0 +1,24 @@
+<?php
+include "includes/header.php";
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+    $username = htmlentities($username);
+    $password = htmlentities($password);
+
+    $sql = "select password from users where username='$username'";
+    $res = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_assoc($res);
+    $pass = $row['password'];
+    if(password_verify($password,$pass)){
+        header("Location: dashboard.php");
+    } else {
+        header("Location: login.php");
+        $_SESSION['message'] = "<div class='chip red black-text'>Sorry, Credentials Don,t Match</div>";
+    }
+    
+}
+?>    
